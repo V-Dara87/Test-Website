aa279443e3983c1da3d80746294ee7074a3017e3
V-Dara87 <TobyMarute@gmail.com>
2026-06-09 10:30:46 +0700

    Add button and  more text into home menu

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/Test-Website/',   // 👈 add this line
   plugins: [
     tailwindcss(),
   ],
